2b70faefd22888b503bcb97f41039f971553a506
Ryan Cramer <ryan@rc-d.net>
2015-05-25 07:30:04 -0400

    Additional updates to make blog profile a native PW 2.5 site profile

--- site-blog/install/info.php
+++ site-blog/install/info.php
@@ -1,6 +1,6 @@
 <?php if(!defined("PROCESSWIRE_INSTALL")) die();
 $info = array(
 	'title' => "Blog Profile", 
-	'summary' => "This is a basic responsive blog profile designed to get you started quickly. The profile can be used as-is or modified to suit your needs.", 
+	'summary' => "A response blog profile for ProcessWire 2.5 or newer.", 
 	'screenshot' => "screenshot.jpg"
 	);
